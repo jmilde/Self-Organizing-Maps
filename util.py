060390f245aeeac16613e2fd54d9da8a21241494
jmilde <35452916+jmilde@users.noreply.github.com>
2019-02-12 23:02:09 +0100

    added analyze

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+from collections import Counter
 
 def normalize(raw_data, normalize_by_column=False):
     # if True, assume all data is on common scale
@@ -38,3 +39,33 @@ def profile(sess, wrtr, run, feed_dict= None, prerun= 5, tag= 'flow'):
     meta = tf.RunMetadata()
     sess.run(run, feed_dict, tf.RunOptions(trace_level= tf.RunOptions.FULL_TRACE), meta)
     wrtr.add_run_metadata(meta, tag)
+
+
+def analyze(labels, code_lbl, threshold, counts, max_acc=1):
+    """
+          labels: cluster map
+        code_lbl: list of 0/1 depending on if the Code appears or not
+        treshold: 0.xx how many % of a cluster have to be of the chosen code for it to become relevant
+          counts: how many instances must be in a cluster
+         max_acc: only clusters with an accuracy smaller than max_acc are selected
+
+    return: pred_counts = number of instances per cluster
+             pred_acc    = % of instances in the cluster being of the code
+             best_pred   = cluster above treshold and count
+    """
+    n_clusters = len(set(labels))
+
+    pred_match = np.zeros(n_clusters)
+    for idx, label in enumerate(labels):
+        if code_lbl[idx] == 1:
+            pred_match[label] += 1
+
+    # array that counts how many instances are in the cluster
+    pred_counts = np.array([x[1] for x in list(Counter(labels).items())])
+
+    pred_acc = np.array([(b/a) if b!=0 else 0 for a, b in zip(pred_counts, pred_match)])
+
+    best_pred = [[idx, x, pred_counts[idx]] for idx, x in enumerate(pred_acc)
+                 if x>threshold and pred_counts[idx]>counts and x<max_acc]
+
+    return pred_counts, pred_acc, best_pred
